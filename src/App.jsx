883df6222212034141d1379f5dda38eb6143cb0e
John Alber Santos <santos1802909@ceu.edu.ph>
2021-10-18 13:12:34 +0800

    Add Workflow

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1><span role="img" aria-label="title">⚛️🔥💬</span></h1>
         <SignOut />
       </header>
 
@@ -101,7 +101,7 @@ function ChatRoom() {
 
         <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
-        <button type="submit" disabled={!formValue}>🕊️</button>
+        <button type="submit" disabled={!formValue}><span role="img" aria-label="enter">🕊️</span></button>
 
       </form>
     </>
